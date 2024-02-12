bc2c4a35734bb1674a6490a016980d978e50263c
Steinshy <nekoheartdev@gmail.com>
2024-02-12 19:12:52 +0100

    add axios + setup dummy api + display

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setData] = useState([]);
+  const URL = "https://dummyjson.com/products";
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(URL);
+      setData(response.data.products);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Display API data from Dummy API</h1>
+      <tbody>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.description}</td>
+            <td>{item.price}</td>
+            <td>{item.category}</td>
+          </tr>
+        ))}
+      </tbody>
+    </div>
+  );
+};
 
-export default App
+export default App;
