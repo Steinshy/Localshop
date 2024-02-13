f2f981309b3112f057f5ed8d3f5d0f3d8cf41b4b
Steinshy <nekoheartdev@gmail.com>
2024-02-13 18:40:07 +0100

    design card yay

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
 import axios from "axios";
 
 // Data being passed to useState and return expected data
@@ -27,18 +28,39 @@ const ProductList = () => {
     fetchData();
   }, [fetchData]);
 
-  return (
-    <div>
-      <h1>Products Lists</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.title} - {product.price}
-          </li>
-        ))}
-      </ul>
+  return products.map((product) => (
+    <div className="max-w-[900px] gap-2 grid grid-cols-2 grid-rows-2 px-8">
+      <Card
+        key={product.id}
+        isFooterBlurred
+        className="w-full h-[300px] col-span-12 sm:col-span-5"
+      >
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">New</p>
+          <h4 className="text-white font-medium text-2xl">{product.title}</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          isZoomed
+          alt="Card example background"
+          className="z-0 w-500 h-264 object-cover"
+          src={product.thumbnail}
+        />
+        <CardFooter className="absolute bg-black/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+          <div>
+            <p className="text-white text-small">{product.price} €</p>
+          </div>
+          <Button className="text-tiny" color="primary" radius="full" size="sm">
+            Buy
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
-  );
+  ));
+  //         <li>{product.title}</li>
+  //         <li>{product.brand}</li>
+  //         <li>{product.category}</li>
+  //         <li>{product.description}</li>
 };
 
 export default ProductList;
