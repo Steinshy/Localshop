1d1f5d0c41bcb05b667d6401db77979a5430e07c
Steinshy <nekoheartdev@gmail.com>
2024-02-20 17:43:48 +0100

    fix card mobile

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -33,7 +33,7 @@ const ProductList = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-2 p-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 p-2">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} isLoading={isLoading} />
       ))}
