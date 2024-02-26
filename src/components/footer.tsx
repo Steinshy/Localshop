4cf6802fe17046a9792c7faf17e690774501afde
Steinshy <nekoheartdev@gmail.com>
2024-02-26 15:19:06 +0100

    add divider to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,11 @@
+import { Divider } from "@chakra-ui/react";
+
 export default function Footer() {
   return (
-    <footer className="text-gray-800 w-4/5 mx-auto inter md:pt-24">
-      <div className="flex items-center mx-auto text-gray-600 container justify-center md:justify-center py-2">
-        <span className="font-normal text-center">
-          © Copyright 2024, All Rights Reserved
-        </span>
+    <footer className="">
+      <Divider />
+      <div className="">
+        <span className="">© Copyright 2024, All Rights Reserved</span>
       </div>
     </footer>
   );
