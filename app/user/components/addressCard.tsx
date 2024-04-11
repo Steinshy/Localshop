b9c5fb782020c5005c3f594d3f74588f143a0a03
Steinshy <asemarealis@gmail.com>
2024-04-11 18:35:39 +0200

    add default address style

--- app/user/components/addressCard.tsx
+++ app/user/components/addressCard.tsx
@@ -2,7 +2,7 @@
 import { FC } from "react";
 
 // NextUI
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody, Chip } from "@nextui-org/react";
 
 // Components
 import AddressModal from "../../user/components/addressModal";
@@ -11,35 +11,42 @@ import RemoveAddress from "../../user/components/removeAddress";
 // Interfaces
 import { AddressCardProps } from "../../utils/interfaces";
 
-const AddressCard:FC<AddressCardProps> = ({ selected, setSelected, address, selectable = false }) => {
-
+const AddressCard: FC<AddressCardProps> = ({ selected, setSelected, address, selectable = false }) => {
   const handleSelect = () => {
     if (setSelected) {
       setSelected(address.id);
     }
-  }
+  };
 
   return (
     <div className="relative h-[124px]">
       <Card
-        className={`border-2 w-full h-full ${selected === address.id ? 'border-primary' : 'border-transparent'}`}
+        className={`border-2 w-full h-full ${selected === address.id ? "border-primary" : "border-transparent"}`}
         isPressable={selectable}
         onClick={handleSelect}
       >
         <CardBody>
           <h2 className="font-semibold">{address.label}</h2>
-          <p>{address.firstname} {address.lastname}</p>
+          <p>
+            {address.firstname} {address.lastname}
+          </p>
           <p>{address.address}</p>
-          <p>{address.city}, {address.country} {address.postalCode}</p>
-          {/* <p>{address.default ? "Default" : ""}</p> */}
+          <p>
+            {address.city}, {address.country} {address.postalCode}
+          </p>
         </CardBody>
       </Card>
       <div className="absolute top-2 right-2 flex gap-1">
+        {address.default && (
+          <Chip variant="flat" color="primary" size="sm" radius="sm">
+            Default
+          </Chip>
+        )}
         <AddressModal id={address.id} />
         <RemoveAddress id={address.id} />
       </div>
     </div>
   );
-}
+};
 
 export default AddressCard;
