ee837b29527de0aa9705fff47613f5b816fbef4a
Steinshy <asemarealis@gmail.com>
2024-04-11 18:22:13 +0200

    add shippingPrice + taxesPrice const for order + login checkup

--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -4,17 +4,20 @@
 import { useContext, useEffect, useState } from "react";
 
 // Components - Generation
-import { CartContext } from "../utils/subProviders";
+import { CartContext, UserContext } from "../utils/subProviders";
 import CartSummary from "./cart/components/cartSummary";
 
-export default function Cart({ children }: {children: React.ReactNode}) {
+export default function Cart({ children }: { children: React.ReactNode }) {
   // Cart Store context
   const cartStore = useContext(CartContext);
+  const userStore = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(true);
   // Cart
   const [cartChecked, setCartChecked] = useState(false);
   const [cart, setCart] = useState(cartStore.data);
   const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [shippingPrice, setshippingPrice] = useState<number>(0);
+  const [taxesPrice, setTaxesPrice] = useState<number>(0);
 
   // Calculate Total Price
   useEffect(() => {
@@ -41,10 +44,14 @@ export default function Cart({ children }: {children: React.ReactNode}) {
     setIsLoading(false);
   }, [cartChecked]);
 
-  return (
+  return userStore.isLogged() ? (
     <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-8">
       {children}
-      <CartSummary cart={cart} totalPrice={totalPrice} isLoading={isLoading} />
+      <CartSummary cart={cart} totalPrice={totalPrice} shippingPrice={shippingPrice} taxesPrice={taxesPrice} isLoading={isLoading} />
+    </div>
+  ) : (
+    <div className="flex flex-col flex-grow items-center justify-center">
+      <h1 className="text-md text-heading xl:text-lg lg:text-md">You need to be logged in to view your cart.</h1>
     </div>
   );
 }
