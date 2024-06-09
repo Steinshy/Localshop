c5319aa15a7c7265c156668be8cd36fbddabf65d
Steinshy <asemarealis@gmail.com>
2024-06-10 01:48:51 +0200

    add couponCode to data

---
+++ app/data/couponCode.tsx
@@ -0,0 +1,39 @@
+// Rob
+
+[
+  {
+    "id": 1,
+    "category_id": null,
+    "code": "US_RYN4ERASEJQ",
+    "discount": 10,
+    "expiration": "2024-07-07T12:20:25.978Z",
+  },
+  {
+    "id": 2,
+    "category_id": null,
+    "code": "D_WMNSQLO0GRPW",
+    "discount": 25,
+    "expiration": "2024-07-07T12:20:26.016Z",
+  },
+  {
+    "id": 3,
+    "category_id": null,
+    "code": "QKGUCMQTX-ICHG",
+    "discount": 50,
+    "expiration": "2024-07-07T12:20:26.023Z",
+  },
+  {
+    "id": 4,
+    "category_id": null,
+    "code": "IJERADTZAIN1YG",
+    "discount": 75,
+    "expiration": "2024-07-07T12:20:26.028Z",
+  },
+  {
+    "id": 5,
+    "category_id": null,
+    "code": "YGYNCPWJGPH3KG",
+    "discount": 100,
+    "expiration": "2024-07-07T12:20:26.035Z",
+  }
+]
