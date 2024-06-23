2117a663940626a292e73ce0cfd0622ec4192d71
Steinshy <asemarealis@gmail.com>
2024-06-23 18:52:14 +0200

    add unlighthouse to dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     domains: ["url"],
   },
+  output: 'export',
 };
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            "key": "Content-Security-Policy",
+            "value": "default-src 'self' https: ; script-src 'self' ; object-src 'none'"
+          },
+        ],
+      },
+    ]
+  },
+}
+
 
 export default nextConfig;
