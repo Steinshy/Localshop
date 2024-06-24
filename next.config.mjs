0f7da60f3769f497e3e7c784dcb2bcdf472e64dc
Steinshy <asemarealis@gmail.com>
2024-06-24 14:30:20 +0200

    remove headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,10 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 
 const nextConfig = {
   images: {
-    domains: ["url"],
+    domains: ['url'],
   },
   output: 'export',
 };
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/about',
-        headers: [
-          {
-            "key": "Content-Security-Policy",
-            "value": "default-src 'self' https: ; script-src 'self' ; object-src 'none'"
-          },
-        ],
-      },
-    ]
-  },
-}
-
 
 export default nextConfig;
