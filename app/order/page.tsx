c80734f41e238bae5649a00d2c8549f24002d8f7
Steinshy <asemarealis@gmail.com>
2024-07-08 19:16:26 +0200

    fix start shopping button

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -8,8 +8,6 @@ import Link from 'next/link';
 
 // NextUI
 import { Button } from '@nextui-org/react';
-
-// Icons
 import { FaCartArrowDown, FaArrowRight } from 'react-icons/fa';
 
 // Components
@@ -31,7 +29,7 @@ const OrderPage = () => {
       ))}
     </ul>
   ) : (
-    <div className='flex flex-col flex-grow items-center justify-center'>
+    <div className='flex flex-col flex-grow justify-center items-center sm:col-span-7 gap-2'>
       <FaCartArrowDown className='text-8xl text-foreground' />
       <p className='text-lg text-center mt-4'>Your cart is empty</p>
       <Button color='primary' variant='flat' href='/products' as={Link} className='mt-4' endContent={<FaArrowRight />}>
