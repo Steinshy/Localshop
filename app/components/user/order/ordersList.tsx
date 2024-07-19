79116baf690fc51ab95ede4c0ac483d6552ad957
Steinshy <asemarealis@gmail.com>
2024-07-19 19:25:05 +0200

    add continue shopping + cartClearBtn in order page + cleanup ordersList

--- app/components/user/order/ordersList.tsx
+++ app/components/user/order/ordersList.tsx
@@ -13,22 +13,22 @@ import OrderCard from '@components/user/order/orderCard';
 import { FaSearch } from 'react-icons/fa';
 
 // Actions
-import { getOrders } from '@actions/actionsUserOrders'
+import { getOrders } from '@actions/actionsUserOrders';
+
+// Interfaces
 import { OrderListProps, OrderResponse } from '@interfaces/userOrder';
+import { PagyProps } from '@interfaces/general';
+import { ErrorObj } from '@interfaces/httpUtils';
 
 // Utils
 import { showToast } from '@utils/helpers';
 
-// Interfaces
-import { PagyProps } from '@interfaces/general';
-import { ErrorObj } from '@interfaces/httpUtils';
-
 const OrdersList: FC<OrderListProps> = ({ items = [], pageInfos = { page: 1, pages: 1 }, pageError }) => {
   const [orders, setOrders] = useState<OrderResponse[]>(items),
-        [isFetching, setIsFetching] = useState<boolean>(false),
-        [query, setQuery] = useState<string>(''),
-        [pagy, setPagy] = useState<PagyProps>(pageInfos),
-        [error, setError] = useState<Error | ErrorObj | string | undefined>(pageError);
+    [isFetching, setIsFetching] = useState<boolean>(false),
+    [query, setQuery] = useState<string>(''),
+    [pagy, setPagy] = useState<PagyProps>(pageInfos),
+    [error, setError] = useState<Error | ErrorObj | string | undefined>(pageError);
 
   const fetch = (page?: number, query?: string) => {
     const apiFetch = async () => {
@@ -42,11 +42,11 @@ const OrdersList: FC<OrderListProps> = ({ items = [], pageInfos = { page: 1, pag
 
       setOrders(data);
       setPagy(pagy);
-    }
+    };
 
     setIsFetching(true);
     void apiFetch();
-  }
+  };
 
   const handleSearch = (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -60,11 +60,11 @@ const OrdersList: FC<OrderListProps> = ({ items = [], pageInfos = { page: 1, pag
 
   const handlePreviousPage = () => {
     void fetch(pagy.page - 1, query);
-  }
+  };
 
   const handleNextPage = () => {
     void fetch(pagy.page + 1, query);
-  }
+  };
 
   return (
     <>
@@ -72,21 +72,10 @@ const OrdersList: FC<OrderListProps> = ({ items = [], pageInfos = { page: 1, pag
         <h1 className='text-2xl'>Orders</h1>
         {/* Search form */}
         <form onSubmit={handleSearch} className='flex flex-grow justify-center items-center px-2'>
-          <Input
-            aria-label='Search'
-            placeholder='Type...'
-            type='search'
-            value={query}
-            onValueChange={setQuery}
-            onClear={handleClear}
-            startContent={<FaSearch />}
-            size='md'
-            isDisabled={isFetching}
-            isClearable
-          />
+          <Input aria-label='Search' placeholder='Type...' type='search' value={query} onValueChange={setQuery} onClear={handleClear} startContent={<FaSearch />} size='md' isDisabled={isFetching} isClearable />
         </form>
       </div>
-      
+
       <div className='flex flex-col flex-grow'>
         {orders.length > 0 && !error && !isFetching ? (
           <div className='grid grid-cols-1 gap-3'>
@@ -96,26 +85,22 @@ const OrdersList: FC<OrderListProps> = ({ items = [], pageInfos = { page: 1, pag
           </div>
         ) : (
           <div className='flex flex-col flex-grow items-center justify-center'>
-            {isFetching ?
-              <Spinner color='primary' size='lg' />
-            :
-              <p className='text-md break-all'>
-                {error ?
-                  'There was an error retrieving your orders'
-                :
-                  query.length > 0 ? `No results for ${query}` : 'No orders has been made yet'
-                }
-              </p>
-            }
+            {isFetching ? <Spinner color='primary' size='lg' /> : <p className='text-md break-all'>{error ? 'There was an error retrieving your orders' : query.length > 0 ? `No results for ${query}` : 'No orders has been made yet'}</p>}
           </div>
         )}
       </div>
 
       {/* Pagination */}
       <div className='flex justify-between items-center'>
-        <Button size='sm' isDisabled={pagy.page <= 1 || isFetching} onPress={handlePreviousPage}>Previous</Button>
-        <p className='text-sm text-foreground/50'>Displaying page {pagy.page} of {pagy.pages}</p>
-        <Button size='sm' isDisabled={pagy.page === pagy.pages || isFetching} onPress={handleNextPage}>Next</Button>
+        <Button size='sm' isDisabled={pagy.page <= 1 || isFetching} onPress={handlePreviousPage}>
+          Previous
+        </Button>
+        <p className='text-sm text-foreground/50'>
+          Displaying page {pagy.page} of {pagy.pages}
+        </p>
+        <Button size='sm' isDisabled={pagy.page === pagy.pages || isFetching} onPress={handleNextPage}>
+          Next
+        </Button>
       </div>
     </>
   );
--- app/order/page.tsx
+++ app/order/page.tsx
@@ -8,10 +8,13 @@ import Link from 'next/link';
 
 // NextUI
 import { Button } from '@nextui-org/react';
-import { FaCartArrowDown, FaArrowRight } from 'react-icons/fa';
+
+// Icons
+import { FaCartArrowDown, FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 
 // Components
 import CartProduct from '@components/cart/cartProduct';
+import CartClearBtn from '@components/cart/cartClearBtn';
 
 // Utils
 import { CartContext } from '@subProviders/cartProvider';
@@ -23,6 +26,12 @@ const OrderPage = () => {
 
   return items.length ? (
     <div className='flex flex-col sm:col-span-7 gap-2'>
+      <div className='flex justify-between'>
+        <Button color='default' variant='light' href='/products' as={Link} startContent={<FaArrowLeft className='text-foreground/50' />} className='text-foreground/50'>
+          Continue shopping
+        </Button>
+        <CartClearBtn />
+      </div>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-2'>
         {items.map((cartItem) => (
           <CartProduct key={cartItem.id} cartItem={cartItem} />
@@ -33,7 +42,7 @@ const OrderPage = () => {
     <div className='flex flex-col flex-grow justify-center items-center sm:col-span-7 gap-2'>
       <FaCartArrowDown className='text-8xl text-foreground' />
       <p className='text-lg text-center mt-4'>Your cart is empty</p>
-      <Button color='primary' variant='flat' href='/products' as={Link} className='mt-4' endContent={<FaArrowRight />}>
+      <Button color='primary' variant='flat' href='/products' as={Link} endContent={<FaArrowRight />}>
         Start shopping
       </Button>
     </div>
